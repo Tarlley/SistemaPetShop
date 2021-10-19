43b688ea9d2798daeeee6e0a640f0baa5a0edaa0
Wendel Tarlley <wendeltarlley@gmail.com>
2021-10-19 13:29:51 -0300

    Adicionado a classe ItemPedido e ItemPedidoPK

---
+++ src/main/java/br/com/devtarlley/DenguinhosPetShop/domains/ItemPedido.java
@@ -0,0 +1,11 @@
+package br.com.devtarlley.DenguinhosPetShop.domains;
+
+public class ItemPedido {
+
+    private Double desconto;
+    private Integer quantidade;
+    private Double preco;
+
+    private ItemPedidoPK id = new ItemPedidoPK();
+
+}
---
+++ src/main/java/br/com/devtarlley/DenguinhosPetShop/domains/ItemPedidoPK.java
@@ -0,0 +1,7 @@
+package br.com.devtarlley.DenguinhosPetShop.domains;
+
+public class ItemPedidoPK {
+
+    private Item item;
+    private Pedido pedido;
+}
